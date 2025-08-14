57bb60fec4b71d9d668a2a45e209d69e97804c72
fercholopex <ferchosanchex1@gmail.com>
2025-08-14 05:01:18 -0600

    Update amplify.js

--- src/config/amplify.js
+++ src/config/amplify.js
@@ -4,7 +4,7 @@ Amplify.configure({
     endpoints: [
       {
         name: 'trafficAPI',
-        endpoint: process.env.NEXT_PUBLIC_API_URL || 'https://main.ddwo4j04n8ass.amplifyapp.com',
+        endpoint: process.env.NEXT_PUBLIC_API_URL || 'https://main.ddwo4j04n8ass.amplifyapp.com/',
         region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1'
       }
     ]
